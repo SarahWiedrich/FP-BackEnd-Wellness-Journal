eb5fd8ee42fd27690dfa660914b392e272a5fdf1
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-08-14 15:44:53 -0500

    connected to mongooose and added middleware

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 const express = require('express')
-const { default: mongoose } = require('mongoose')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const PORT = process.env.PORT || 8080
 
 //express app
 const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//routes
 
 
 //connect to mongoose
